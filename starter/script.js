41749216529e3165e4c3a54ec8d630fe56ba61bb
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2023-01-06 19:58:17 +0000

    Updated JS code

--- starter/script.js
+++ starter/script.js
@@ -2,17 +2,14 @@
 var specialCharacters = [
   '@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
 ];
-
 // Array of numeric characters to be included in password
 var numericCharacters = [
   '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
 ];
-
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ];
-
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
@@ -25,12 +22,8 @@ var confirmNumericCharacters;
 var confirmLowerCasedCharacters;
 var confirmUpperCasedCharacters;
 
-// Function to prompt user for password options confirm lenght
-function getPasswordOptions() {}
-
-
-
-
+// Function to prompt user for password options confirm lenght and characters
+function getPasswordOptions() {
   var confirmLength = (prompt("How many characters of password would you like? \nChoose between 10 up to 64"));
   // Loop if answer is outside parameters
   // The 'while' loop, loops through a block of code as long as a specified condition is true
@@ -42,7 +35,7 @@ function getPasswordOptions() {}
   // Confirm back the answer of how many characters that user will have
   alert("Your password length is " + confirmLength + " characters");
 
-  // Determine parameters of password 
+  // Determine characters of password 
   var confirmSpecialCharacters = confirm("Would you like to include \nSpecial characters?" );
   var confirmNumericCharacters = confirm("Would you like to include \nNumeric characters?" );
   var confirmLowerCasedCharacters = confirm("Would you like to include \nLowercase characters?" );
@@ -56,6 +49,39 @@ function getPasswordOptions() {}
     var confirmLowerCasedCharacters = confirm("Would you like to include \nLowercase characters?" );
     var confirmUpperCasedCharacters = confirm("Would you like to include \nUppercase characters?" );
   }
+  return getPasswordOptions; //Optional
+}
+
+    // Assign an action to the password parameters
+    var passwordCharacters = []
+    if (confirmSpecialCharacters) {
+      passwordCharacters = passwordCharacters.concat(specialCharacters)
+    }
+    if (confirmNumericCharacters) {
+      passwordCharacters = passwordCharacters.concat(numericCharacters)
+    }
+    if (confirmLowerCasedCharacters) {
+      passwordCharacters = passwordCharacters.concat(lowerCasedCharacters)
+    }
+    if (confirmUpperCasedCharacters) {
+      passwordCharacters = passwordCharacters.concat(upperCasedCharacters)
+    }
+    // Logs password characters
+    console.log (passwordCharacters)
+
+    // Empty string, to be fill base on for loop selected random characters from array
+    var randomPassword = ""
+
+    for (var i = 0; i < confirmLength; i++) {
+      randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+
+      console.log(randomPassword)
+    }
+
+//  return randomPassword;
+
+// Call getPasswordOptions function
+getPasswordOptions();
 
 // Function for getting a random element from an array
 function getRandom(arr) {
