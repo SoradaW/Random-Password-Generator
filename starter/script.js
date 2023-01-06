9de6623fb2fd4fb91d7431fb503ad4f533af8518
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2023-01-06 19:29:28 +0000

    Updated JS code

--- starter/script.js
+++ starter/script.js
@@ -26,17 +26,22 @@ var confirmLowerCasedCharacters;
 var confirmUpperCasedCharacters;
 
 // Function to prompt user for password options confirm lenght
-function getPasswordOptions() {
-  var confirmLength = (prompt("How many characters of password would you like? Choose between 10 up to 64"));
+function getPasswordOptions() {}
+
+
+
+
+  var confirmLength = (prompt("How many characters of password would you like? \nChoose between 10 up to 64"));
   // Loop if answer is outside parameters
   // The 'while' loop, loops through a block of code as long as a specified condition is true
   while(confirmLength <=10 || confirmLength >= 64) {
-  alert("Password lenght must be between 10-64 characters \nTry again!");
-  var confirmLength = (prompt("How many characters of password would you like? Choose between 10 up to 64"));
+  alert("Password length must be between 10-64 characters \nTry again!");
+  // Try again!
+  var confirmLength = (prompt("How many characters of password would you like? \nChoose between 10 up to 64"));
   }
-  // Repeat back user answer // option
-  alert("Your password will be ${confirmLenght} characters");
-}
+  // Confirm back the answer of how many characters that user will have
+  alert("Your password length is " + confirmLength + " characters");
+
   // Determine parameters of password 
   var confirmSpecialCharacters = confirm("Would you like to include \nSpecial characters?" );
   var confirmNumericCharacters = confirm("Would you like to include \nNumeric characters?" );
@@ -45,16 +50,12 @@ function getPasswordOptions() {
   // Loop if answer is outside parameters
   while(confirmSpecialCharacters === false && confirmNumericCharacters === false && confirmLowerCasedCharacters === false && confirmUpperCasedCharacters === false) {
     alert ("At least one character type should be selected \nTry again!");
-    // Loop again
+    // Try again!
     var confirmSpecialCharacters = confirm("Would you like to include \nSpecial characters?" );
     var confirmNumericCharacters = confirm("Would you like to include \nNumeric characters?" );
     var confirmLowerCasedCharacters = confirm("Would you like to include \nLowercase characters?" );
     var confirmUpperCasedCharacters = confirm("Would you like to include \nUppercase characters?" );
   }
-
-
-
-
 
 // Function for getting a random element from an array
 function getRandom(arr) {
